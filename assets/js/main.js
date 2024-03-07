05c44df229306526edd3eebd26b9f493d9f97966
Виктория Захарова <zaharova181114@gmail.com>
2024-03-07 14:22:22 +0200

    catalog pc

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,3 +148,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+
+// amount
+$('.down').on("click", function () {
+	let $input = $(this).parent().find('input');
+	let count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+	$input.change();
+	return false;
+});
+$('.up').on("click", function () {
+	let $input = $(this).parent().find('input');
+	$input.val(parseInt($input.val()) + 1);
+	$input.change();
+	return false;
+});
